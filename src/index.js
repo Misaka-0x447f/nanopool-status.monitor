7d547e009b3623215745d9bd703df697d36ba85f
苍崎橙子 <AozakiOrenji@outlook.com>
2017-09-24 23:33:22 +0800

    front page init

---
+++ src/index.js
@@ -0,0 +1,81 @@
+function update(){
+    config = getConfig();
+    arg = window.location.href + "/../api/interface.php?coinType=" + config["coinType"] + "&address=" + config["address"];
+
+    if(config["coinType"] === "etc"){
+        document.getElementById("balance-unit").innerHTML = "mETC";
+        document.getElementById("hashrate-unit").innerHTML = "MHash/s";
+        document.getElementById("avgHashrate-unit").innerHTML = "MHash/s";
+        document.getElementById("estimatedEarnings-unit").innerHTML = "mETC/day";
+    }
+
+    //1.update balance
+    console.log("requesting");
+    $.ajax({
+        type: "POST",
+        contentType: "application/x-www-form-urlencoded",
+        dataType: "html",
+        url: arg + "&dataType=balance",
+        success: function(data){
+            data = JSON.parse(data);
+            console.log(data);
+            if(data["status"] === "ok"){
+                document.getElementById("balance").innerHTML = (data["balance"] * 1000).toFixed(config["accu"]);
+            }
+        },
+        timeout:90000
+    });
+
+    //2.update hashrate
+    $.ajax({
+        type: "POST",
+        contentType: "application/x-www-form-urlencoded",
+        dataType: "html",
+        url: arg + "&dataType=hashrate",
+        success: function(data){
+            data = JSON.parse(data);
+            console.log(data);
+            if(data["status"] === "ok"){
+                document.getElementById("hashrate").innerHTML = data["hashrate"];
+            }
+        },
+        timeout:90000
+    });
+
+    //3.update avgHashrate
+    $.ajax({
+        type: "POST",
+        contentType: "application/x-www-form-urlencoded",
+        dataType: "html",
+        url: arg + "&dataType=avgHashrate",
+        success: function(data){
+            data = JSON.parse(data);
+            if(data["status"] === "ok"){
+                console.log(data);
+                if(data["status"] === "ok"){
+                    document.getElementById("avgHashrate").innerHTML = data["avgHashrate"];
+                }
+                lastAvgSpeed = data["avgHashrate"];
+            }
+        },
+        timeout:90000
+    });
+
+    //4.update calc
+    if(typeof(lastAvgSpeed) !== "undefined"){
+        $.ajax({
+            type: "POST",
+            contentType: "application/x-www-form-urlencoded",
+            dataType: "html",
+            url: arg + "&dataType=estimatedEarnings" + "&speed=" + lastAvgSpeed,
+            success: function(data){
+                data = JSON.parse(data);
+                console.log(data);
+                if(data["status"] === "ok"){
+                    document.getElementById("estimatedEarnings").innerHTML = data["estimatedEarnings"]["day"]["coins"]*1000;
+                }
+            },
+            timeout:90000
+        });
+    }
+}
--- src/index.php
+++ src/index.php
@@ -1,8 +1,77 @@
-<meta charset="utf-8">
 <?php
 /**
  * Created by PhpStorm.
+ * Silence is gold.
  * User: Aozak
  * Date: 2017/9/24
  * Time: 17:18
  */
+/*require("api/pool.php");
+$poolOpt = new nanopoolEtcEth("etc", "57cda5f152e5da5c16952bc810ebee8235df7353");
+$avgHashrate = $poolOpt->minerAverageHashrate(24)["avgHashrate"];
+echo json_encode($poolOpt->minerEstimatedEarnings($avgHashrate));*/
+require("functions.php");
+$fileOpt = new fileOpt();
+$fileOpt->fileSelect("config.json");
+$config = $fileOpt->jsonFileRead();
+if(isset($_GET["config"]) and property_exists($config, $_GET["config"])){
+    $config = ((array)$config)[$_GET["config"]];
+}else if(isset(((array)$config)["default"])){
+    $config = ((array)$config)["default"];
+}else{
+    exit("{\"status\":\"interrupted\",\"statusNo\":\"400.0\",\"message\":\"bad request. params may invalid.\"}");
+}
+?>
+<meta charset="utf-8">
+<title>
+    sitBackAndControl
+</title>
+<link href="index.css" rel="stylesheet">
+<script src="index.js<?php echo "?nocache=" . (string)rand() ?>"></script>
+<script src="lib/jquery.js"></script>
+<script>
+    $(document).ready(function(){
+        setTimeout(update,3*1000);
+        setInterval(update,15*1000);
+    });
+    <?php
+        echo '
+            function getConfig(){
+                var config = ' . (string)json_encode($config) . ';
+                console.log(config);
+                return config;
+            }
+        '
+    ?>
+</script>
+<body>
+<div id="container">
+    <span id="balance">
+        ---.-
+    </span>
+    <span id="balance-unit">
+
+    </span>
+    <br/>
+    <span id="hashrate">
+
+    </span>
+    <span id="hashrate-unit">
+
+    </span>
+    <br/>
+    <span id="avgHashrate">
+
+    </span>
+    <span id="avgHashrate-unit">
+
+    </span>
+    <br/>
+    <span id="estimatedEarnings">
+
+    </span>
+    <span id="estimatedEarnings-unit">
+
+    </span>
+</div>
+</body>
